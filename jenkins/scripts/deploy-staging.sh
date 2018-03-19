f2717305e5162c2a11dda595568cee7de7f13eec
Aitor Fernandez Gomez <afgomez@ideko.es>
2018-03-19 12:22:49 +0100

    sd

--- jenkins/scripts/deploy-staging.sh
+++ jenkins/scripts/deploy-staging.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Staging environment: 31.171.247.162
-# Private key for ssh: /opt/keypairs/ditas-testbed-keypair.pem 
+# Private key for ssh: /opt/keypairs/ditas-testbed-keypair.pem
 
 # TODO state management? We are killing without careing about any operation the conainer could be doing.
 
 
-ssh ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
+ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
 # Ensure that a previously running instance is stopped (-f stops and removes in a single step)
 # || true - "docker stop" failt with exit status 1 if image doen't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0.
 sudo docker rm -f simple-java-maven-app || true'
