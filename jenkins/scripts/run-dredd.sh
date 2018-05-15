e66ee2d68f2600da0c39501ab994a30b9171421b
borjakhet <btornos@ideko.es>
2018-05-15 14:50:39 +0200

    Update run-dredd.sh

--- jenkins/scripts/run-dredd.sh
+++ jenkins/scripts/run-dredd.sh
@@ -2,7 +2,7 @@
 # Staging environment: 31.171.247.162
 # Private key for ssh: /opt/keypairs/ditas-testbed-keypair.pem 
 
-scp -i /opt/keypairs/ditas-testbed-keypair.pem Dockerfile.artifact cloudsigma@31.171.247.162:/home/cloudsigma/dreddtest/Dockerfile.artifact
+#scp -i /opt/keypairs/ditas-testbed-keypair.pem Dockerfile.artifact cloudsigma@31.171.247.162:/home/cloudsigma/dreddtest/Dockerfile.artifact
 
 ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
 
@@ -10,6 +10,7 @@ ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'END
 # sudo ln -s /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/node /usr/bin/node
 
 # Run dredd from the real path
-/home/cloudsigma/.nvm/versions/node/v8.11.1/bin/dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
+# /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
+dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
 
 ENDSSH
