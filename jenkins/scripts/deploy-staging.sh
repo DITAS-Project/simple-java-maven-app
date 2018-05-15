cec50764d25a5a16222eb18f036baf2920e9f839
borjakhet <btornos@ideko.es>
2018-05-15 11:36:28 +0200

    Update deploy-staging.sh

--- jenkins/scripts/deploy-staging.sh
+++ jenkins/scripts/deploy-staging.sh
@@ -11,8 +11,7 @@ ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'END
 echo "hey"
 cd /home/cloudsigma/.nvm/versions/node/v8.11.1/bin
 # ln -s /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/node /usr/bin/node
-# /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
-dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
+/home/cloudsigma/.nvm/versions/node/v8.11.1/bin/dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
 # sudo docker rm -f simple-java-maven-app || true
 # sudo docker pull ditas/simple-java-maven-app:latest
 # sudo docker run -d --name simple-java-maven-app ditas/simple-java-maven-app:latest
