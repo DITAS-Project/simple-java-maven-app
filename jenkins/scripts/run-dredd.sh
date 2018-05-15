79d342732dd60beaaa9c961142eddbe60e62cf15
borjakhet <btornos@ideko.es>
2018-05-15 13:52:55 +0200

    Update run-dredd.sh

--- jenkins/scripts/run-dredd.sh
+++ jenkins/scripts/run-dredd.sh
@@ -2,12 +2,14 @@
 # Staging environment: 31.171.247.162
 # Private key for ssh: /opt/keypairs/ditas-testbed-keypair.pem 
 
-ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
+scp -i /opt/keypairs/ditas-testbed-keypair.pem Dockerfile.artifact cloudsigma@31.171.247.162:/home/cloudsigma/dreddtest/Dockerfile.artifact
+
+# ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
 
 # Create the symbolic link in order to use 'node' as PATH variable
 # sudo ln -s /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/node /usr/bin/node
 
 # Run dredd from the real path
-/home/cloudsigma/.nvm/versions/node/v8.11.1/bin/dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
+#  /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
 
 ENDSSH
