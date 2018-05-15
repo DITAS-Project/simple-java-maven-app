d06c305fa6b8b90856ecc5890062f8f2c451f2ec
borjakhet <btornos@ideko.es>
2018-05-15 12:07:02 +0200

    run-dredd.sh

---
+++ jenkins/scripts/run-dredd.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Staging environment: 31.171.247.162
+# Private key for ssh: /opt/keypairs/ditas-testbed-keypair.pem 
+
+ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
+
+# Create the symbolic link in order to use 'node' as PATH variable
+# sudo ln -s /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/node /usr/bin/node
+
+# Run dredd from the real path
+/home/cloudsigma/.nvm/versions/node/v8.11.1/bin/dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
+
+ENDSSH
