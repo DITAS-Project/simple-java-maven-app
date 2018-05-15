844ac0bb3956a048fb953108e8ca7798cf8b18de
borjakhet <btornos@ideko.es>
2018-05-15 11:41:42 +0200

    Update deploy-staging.sh

--- jenkins/scripts/deploy-staging.sh
+++ jenkins/scripts/deploy-staging.sh
@@ -8,7 +8,7 @@
 ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
 # Ensure that a previously running instance is stopped (-f stops and removes in a single step)
 # || true - "docker stop" failt with exit status 1 if image doen't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0.
-echo "heyt"
+echo "heytuu"
 
 ln -s /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/node /usr/bin/node
 /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
