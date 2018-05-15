bf8fd46743fb32387302a3171bdc1c78750a68da
borjakhet <btornos@ideko.es>
2018-05-15 11:35:54 +0200

    Update deploy-staging.sh

--- jenkins/scripts/deploy-staging.sh
+++ jenkins/scripts/deploy-staging.sh
@@ -9,9 +9,10 @@ ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'END
 # Ensure that a previously running instance is stopped (-f stops and removes in a single step)
 # || true - "docker stop" failt with exit status 1 if image doen't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0.
 echo "hey"
-hostname
-ln -s /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/node /usr/bin/node
-/home/cloudsigma/.nvm/versions/node/v8.11.1/bin/dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
+cd /home/cloudsigma/.nvm/versions/node/v8.11.1/bin
+# ln -s /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/node /usr/bin/node
+# /home/cloudsigma/.nvm/versions/node/v8.11.1/bin/dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
+dredd /home/cloudsigma/dreddtest/apimattia.yaml http://31.171.247.162:50003
 # sudo docker rm -f simple-java-maven-app || true
 # sudo docker pull ditas/simple-java-maven-app:latest
 # sudo docker run -d --name simple-java-maven-app ditas/simple-java-maven-app:latest
